c550129268d5b6bd748b4695cb8d03ad25b5e52b
Fanis Chamalidis <fchamalidis@yahoo.com>
2024-08-12 12:50:17 +0300

    Created Header component

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import logo from '../assets/logodark.svg';
+
+// interface NavbarProps {
+//   isLoggedIn: boolean;
+//   onLogout: () => void;
+// }
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>
+              <img src={logo} id='logo' alt='Office Assistant' />
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <LinkContainer to='/chat'>
+                <Nav.Link className='me-3 fs-4 text-light'>Chat</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link className='me-3 fs-4 text-light'>Login</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/register'>
+                <Nav.Link className='me-3 fs-4 text-light'>Register</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
