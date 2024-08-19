33276edccd21418e409e876e3b72922420a845e3
Fanis Chamalidis <fchamalidis@yahoo.com>
2024-08-19 18:53:41 +0300

    Created frontend chat functionality

--- frontend/src/utils/chatUtils.ts
+++ frontend/src/utils/chatUtils.ts
@@ -3,19 +3,64 @@ export interface Message {
   sender: 'user' | 'bot';
 }
 
-export const processUserMessage = (
+interface Appointment {
+  title: string;
+  participant: string;
+  participantPhoneNumber: number;
+  date: Date;
+}
+
+export const processUserMessage = async (
   message: string,
   setMessages: React.Dispatch<React.SetStateAction<Message[]>>
 ) => {
+  if (!message) return;
+
   const userMessage: Message = { text: message, sender: 'user' };
   setMessages((prevMessages) => [...prevMessages, userMessage]);
 
-  // Simulate bot response
-  setTimeout(() => {
+  try {
+    const response = await fetch('http://localhost:5000/api/chatbot/prompt', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+      body: JSON.stringify({ prompt: message }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    let botMessage: Message = {
+      text: 'You have no appointments',
+      sender: 'bot',
+    };
+
+    const data = await response.json();
+    console.log(data);
+
+    if (data.appointments) {
+      botMessage.text = 'Your appointments are: ';
+      const appointments = data.appointments;
+      appointments.forEach((appointment: Appointment) => {
+        const { title, participant, participantPhoneNumber, date } =
+          appointment;
+        botMessage.text += `${title} appointment with ${participant} on ${date}. ${participant}'s contact phone number is ${participantPhoneNumber}`;
+      });
+    } else {
+      botMessage.text =
+        data.message || data.error || data.response || 'No response';
+    }
+
+    setMessages((prevMessages) => [...prevMessages, botMessage]);
+  } catch (error) {
+    console.error('Error sending prompt to the backend', error);
     const botMessage: Message = {
-      text: `Bot response to: ${message}`,
+      text: 'Sorry, something went wrong.',
       sender: 'bot',
     };
     setMessages((prevMessages) => [...prevMessages, botMessage]);
-  }, 1000);
+  }
 };
